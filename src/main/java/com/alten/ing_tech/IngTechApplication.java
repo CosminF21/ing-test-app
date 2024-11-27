d7ff981d7844da72ac6b9638c8d83e6dd7bf66a6
CosminF21 <cosmin.cofan@alten.com>
2024-11-27 11:38:18 +0200

    first commit

---
+++ src/main/java/com/alten/ing_tech/IngTechApplication.java
@@ -0,0 +1,13 @@
+package com.alten.ing_tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alten/ing_tech/IngTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alten.ing_tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
